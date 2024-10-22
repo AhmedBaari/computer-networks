e1f3a6ccfbbfb871255482eed4d81533707200ed
Ahmed Baari <ahmed4baari@gmail.com>
2024-10-22 10:34:33 +0530

    DHCP further optimized by Shriram

--- 9_DHCP/DHCPClient.java
+++ 9_DHCP/DHCPClient.java
@@ -1,23 +1,20 @@
-// Optimized by Baari
 import java.net.*;
 
 public class DHCPClient {
-    private static final int SERVER_PORT = 4900;
-    private static final String SERVER_IP = "127.0.0.1"; // Change to server's IP
-
     public static void main(String[] args) {
         try (DatagramSocket socket = new DatagramSocket()) {
-            InetAddress serverAddress = InetAddress.getByName(SERVER_IP);
-
+            // Request to server
+            InetAddress serverAddress = InetAddress.getByName("127.0.0.1");
             byte[] requestData = ("DHCP Request with MAC: 00:11:22:33:44:55").getBytes();
-            DatagramPacket requestPacket = new DatagramPacket(requestData, requestData.length, serverAddress,
-                    SERVER_PORT);
+            DatagramPacket requestPacket = new DatagramPacket(requestData, requestData.length, serverAddress, 4900);
             socket.send(requestPacket);
 
+            // Receive
             byte[] receiveData = new byte[1024];
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
             socket.receive(receivePacket);
 
+            // Display
             String response = new String(receivePacket.getData()).trim();
             System.out.println("Received DHCP Response: " + response);
         } catch (Exception e) {
--- 9_DHCP/DHCPServer.java
+++ 9_DHCP/DHCPServer.java
@@ -1,49 +1,36 @@
-// Optimized by Baari
 import java.net.*;
 import java.util.*;
 
 public class DHCPServer {
-    private static final int SERVER_PORT = 4900;
     private static List<String> availableIpAddresses = new ArrayList<>();
 
     public static void main(String[] args) {
-        initializeIpAddresses();
 
-        try (DatagramSocket socket = new DatagramSocket(SERVER_PORT)) {
+        for (int i = 2; i <= 254; i++)
+            availableIpAddresses.add("192.168.1." + i);
+
+        try (DatagramSocket socket = new DatagramSocket(4900)) {
             while (true) {
+                // Receive
                 byte[] receiveData = new byte[1024];
                 DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
                 socket.receive(receivePacket);
 
+                // Extract
                 InetAddress clientAddress = receivePacket.getAddress();
-                String macAddress = extractMacAddress();
-                String allocatedIp = allocateIpAddress(macAddress);
-
+                String allocatedIp = availableIpAddresses.remove(0);
                 byte[] responseData = ("Allocated IP: " + allocatedIp).getBytes();
-                DatagramPacket responsePacket = new DatagramPacket(responseData, responseData.length,
-                        clientAddress, receivePacket.getPort());
+
+                // Response 
+                DatagramPacket responsePacket = new DatagramPacket(responseData, responseData.length, clientAddress,
+                        receivePacket.getPort());
                 socket.send(responsePacket);
 
-                System.out.println("Allocated IP " + allocatedIp + " to MAC " + macAddress);
+                // Display
+                System.out.println("Allocated IP " + allocatedIp + " to MAC " + "00:11:22:33:44:55");
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    private static void initializeIpAddresses() {
-        for (int i = 2; i <= 254; i++) {
-            availableIpAddresses.add("192.168.1." + i);
-        }
-    }
-
-    private static String extractMacAddress() {
-        return "00:11:22:33:44:55"; // Simplified placeholder MAC
-    }
-
-    private static String allocateIpAddress(String macAddress) {
-        if (availableIpAddresses.isEmpty())
-            return "No available IP addresses";
-        return availableIpAddresses.remove(0); // Allocate first available IP
     }
 }
